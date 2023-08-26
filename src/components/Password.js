47da800a2ce91ff1c7da4289ab2ea9d876852ec8
SUNIL SURENDRAN <133184647+SunilSurendran1906@users.noreply.github.com>
2023-08-26 23:59:26 +0530

    Update Password.js

--- src/components/Password.js
+++ src/components/Password.js
@@ -17,7 +17,7 @@ const Password = () => {
 
   const formik = useFormik({
     initialValues: {
-      password: "admin@123",
+      password: "",
     },
     validate: passwordValidate,
     validateOnBlur: false,
